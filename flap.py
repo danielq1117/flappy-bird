e2c882b70604718a06f47be3a180222ed98551fe
Daniel Isaias Medina Quiralte <dimquiralte@gmail.com>
2021-04-13 15:09:41 -0600

    background

--- flap.py
+++ flap.py
@@ -4,11 +4,15 @@ pygame.init()
 pantalla = pygame.display.set_mode((288,512))
 reloj = pygame.time.Clock()
 
+superficie_fondo = pygame.image.load('assets/background-day.png').convert()
+
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       sys.exit()
+
+  pantalla.blit(superficie_fondo, (0,0))
   
   pygame.display.update()
   reloj.tick(60)
