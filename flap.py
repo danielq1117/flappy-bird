902b598972255b3f99a6c689c42ca8e43ad4e875
Daniel Isaias Medina Quiralte <dimquiralte@gmail.com>
2021-04-13 15:08:53 -0600

    setup

--- flap.py
+++ flap.py
@@ -1,1 +1,14 @@
 import pygame, sys, random
+
+pygame.init()
+pantalla = pygame.display.set_mode((288,512))
+reloj = pygame.time.Clock()
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+  
+  pygame.display.update()
+  reloj.tick(60)
