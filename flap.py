8512aa54f683fc61f251c936a064909a8d17ce77
Daniel Isaias Medina Quiralte <dimquiralte@gmail.com>
2021-04-13 17:50:51 -0600

    add bird

--- flap.py
+++ flap.py
@@ -8,6 +8,9 @@ superficie_fondo = pygame.image.load('assets/background-day.png').convert()
 superficie_suelo = pygame.image.load('assets/base.png').convert()
 pos_suelo_x = 0
 
+superficie_ave = pygame.image.load('assets/bluebird-midflap.png').convert()
+rect_ave = superficie_ave.get_rect(center = (50,256))
+
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -15,7 +18,7 @@ while True:
       sys.exit()
 
   pantalla.blit(superficie_fondo, (0,0))
-
+  pantalla.blit(superficie_ave,rect_ave)
   pos_suelo_x -= 1
   pantalla.blit(superficie_suelo,(pos_suelo_x,450))
   pantalla.blit(superficie_suelo,(pos_suelo_x + 288,450))
