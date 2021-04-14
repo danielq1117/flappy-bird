6b22e86881636d2cbadc8be98acbef66a6f73637
Daniel Isaias Medina Quiralte <dimquiralte@gmail.com>
2021-04-13 22:34:52 -0600

    colissions

--- flap.py
+++ flap.py
@@ -20,6 +20,16 @@ def mostrar_tubos(tubos):
       voltear_tubo = pygame.transform.flip(superficie_tubo,False,True)
       pantalla.blit(voltear_tubo,tubo)
 
+def detectar_colisiones(tubos):
+  for tubo in tubos:
+    if rect_ave.colliderect(tubo):
+      return False
+
+  if rect_ave.top <= -50 or rect_ave.bottom >= 450:
+    return False
+
+  return True
+
 pygame.init()
 pantalla = pygame.display.set_mode((288,512))
 reloj = pygame.time.Clock()
@@ -27,6 +37,7 @@ reloj = pygame.time.Clock()
 # Variables del Juego
 gravedad = 0.25
 movimiento_ave = 0
+juego_activo = True
 
 superficie_fondo = pygame.image.load('assets/background-day.png').convert()
 superficie_suelo = pygame.image.load('assets/base.png').convert()
@@ -48,23 +59,32 @@ while True:
       sys.exit()
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_SPACE:
-        movimiento_ave = -7
+        if juego_activo:
+          movimiento_ave = -7
+        else:
+          juego_activo = True
+          lista_tubos.clear()
+          rect_ave.center = (50,256)
+          movimiento_ave = 0
+
     if event.type == SPAWNTUBO:
       lista_tubos.extend(crear_tubo())
 
   pantalla.blit(superficie_fondo, (0,0))
 
-  # Ave
-  movimiento_ave += gravedad
-  rect_ave.centery += movimiento_ave
-  pantalla.blit(superficie_ave,rect_ave)
+  if juego_activo:
+    # Ave
+    movimiento_ave += gravedad
+    rect_ave.centery += movimiento_ave
+    pantalla.blit(superficie_ave,rect_ave)
+    juego_activo = detectar_colisiones(lista_tubos)
 
-  # Tubos
-  lista_tubos = mover_tubos(lista_tubos)
-  mostrar_tubos(lista_tubos)
-  
-  # Suelo
-  pos_suelo_x -= 1
+    # Tubos
+    lista_tubos = mover_tubos(lista_tubos)
+    mostrar_tubos(lista_tubos)
+    
+    # Suelo
+    pos_suelo_x -= 1
   pantalla.blit(superficie_suelo,(pos_suelo_x,450))
   pantalla.blit(superficie_suelo,(pos_suelo_x + 288,450))
   if pos_suelo_x <= -288:
